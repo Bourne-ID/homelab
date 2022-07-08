71343a118659098a0c6f80853cfd882bab909a50
Bourne-ID <cam40902@gmail.com>
2022-07-08 01:57:07 +0000

    Personal Home Lab

--- external/modules/cloudflare/main.tf
+++ external/modules/cloudflare/main.tf
@@ -1,5 +1,5 @@
 data "cloudflare_zone" "zone" {
-  name = "khuedoan.com"
+  name = "homelab.bourne.id"
 }
 
 data "cloudflare_api_token_permission_groups" "all" {}
--- platform/vault/files/generate-secrets/go.mod
+++ platform/vault/files/generate-secrets/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/vault/init
+module git.homelab.bourne.id/khuedoan/homelab/vault/init
 
 go 1.17
 
